e3a2cf6c084e61b2d98589f45b211b95651fdd0f
Lily <acrylicrainbow1922@gmail.com>
2020-06-12 19:36:22 +0100

    Removed test log

--- index.js
+++ index.js
@@ -8,20 +8,16 @@ async function redditFetch(obj) {
     const sort = obj.sort || 'top';
     const targetURL = `https://reddit.com/r/${sub}.json?sort=${sort}&t=week`; 
 
-    console.log(targetURL);
-
     return new Promise((resolve, reject) => {
 
         nfetch(targetURL).then(res => res.json())
         .then(body => {
 
             if (!body || !body.data) return reject(new Error('Unable to find a post.'));
-
             let found = body.data.children;
 
             let randInt = Math.floor(Math.random() * found.length);
             let post = found[randInt].data;
-
             resolve(post);
         });
 
