bdd229cb539a82d747762d1601aff35e28cfdf19
Lily <acrylicrainbow1922@gmail.com>
2020-06-12 19:34:37 +0100

    Primary redditFetch function

---
+++ index.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const nfetch = require('node-fetch');
+
+async function redditFetch(obj) {
+
+    const sub = obj.subreddit.toLowerCase();
+    const sort = obj.sort || 'top';
+    const targetURL = `https://reddit.com/r/${sub}.json?sort=${sort}&t=week`; 
+
+    console.log(targetURL);
+
+    return new Promise((resolve, reject) => {
+
+        nfetch(targetURL).then(res => res.json())
+        .then(body => {
+
+            if (!body || !body.data) return reject(new Error('Unable to find a post.'));
+
+            let found = body.data.children;
+
+            let randInt = Math.floor(Math.random() * found.length);
+            let post = found[randInt].data;
+
+            resolve(post);
+        });
+
+    });
+}
+
+module.exports = redditFetch;
